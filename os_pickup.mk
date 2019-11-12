71ef372463cae402af60bf46e548cc35ada39013
argraur <argraur@gmail.com>
2019-11-12 20:25:45 +0000

    os_pickup: Switch to sdm845
    Change-Id: I3a3c6823461081deabf9e22dfc65abced88213dc
    Signed-off-by: argraur <argraur@gmail.com>

--- os_pickup.mk
+++ os_pickup.mk
@@ -1,4 +1,4 @@
-ifeq ($(PRODUCT_PLATFORM),sm8150)
+ifeq ($(PRODUCT_PLATFORM),sdm845)
 ifneq ($(BUILD_WITHOUT_VENDOR), true)
 LOCAL_PATH := $(call my-dir)
 include $(call first-makefiles-under,$(LOCAL_PATH))
